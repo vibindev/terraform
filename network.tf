3a26ece4f749cb12a0d55ad66b077d2f21ee6454
Vibin Andrews <vibin@vismitech.com>
2017-12-11 19:56:38 +0100

    Initial commit

---
+++ network.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr_block"
+
+#Create a VPC with Terraform
+resource "aws_vpc" "custom_vpc" {
+ cidr_block = "${var.vpc_cidr_block}"
+
+ tags {
+    Name = "Terraform Custom VPC"
+    }
+}
