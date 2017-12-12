09cd90870d5bcdbf51b98fa4db39df71284a2262
Vibin Andrews <vibin@vismitech.com>
2017-12-12 19:33:53 +0100

    training - ec2 with userdata

---
+++ instance.tf
@@ -0,0 +1,20 @@
+# Creating EC2 Instance
+resource "aws_instance" "web-server" {
+ ami = "ami-bf4193c7"
+ instance_type = "t2.micro"
+ tag = "Web Server"
+ user_data = <<USER_DATA
+ #!/bin/bash
+ yum update -y
+ yum install -y httpd24 php56 mysql55-server php56-mysqlnd
+ service httpd start
+ chkconfig httpd on
+ groupadd www
+ usermod -a -G www ec2-user
+ chown -R root:www /var/www
+ chmod 2775 /var/www
+ find /var/www -type d -exec chmod 2775 {} +
+ find /var/www -type f -exec chmod 0664 {} +
+ echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+ USER_DATA
+}
